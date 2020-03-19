022830134dba2cf79749cde7a3005b8a6197a729
jw-pyo <wjddn1801@snu.ac.kr>
2020-03-19 10:56:19 +0900

    add nvidia driver option, sshd

--- docker_start.sh
+++ docker_start.sh
@@ -1,8 +1,8 @@
-docker build -t jwpyo_image:latest .
+#docker build -t jwpyo_nvidiadriver:latest .
 
 # docker run -it -d -p 30033:22 -v /hd4/jungwoo_pyo/jungwoo_pyo:/home/jungwoo_pyo --name jwpyo_container jwpyo_image
-docker run -it -d -p 30033:22 --gpus all -v /hd4/table_recognition_dataset:/hd4/table_recognition_dataset --name jwpyo_container jwpyo_image
+docker run -it -d -p 30033:22 --gpus all -v /hd4/table_recognition_dataset:/hd4/table_recognition_dataset --name jwpyo_container jwpyo_nvidiadriver
 #docker run -it -d -p 30033:22 --name jwpyo_container jwpyo_image
-
+docker start jwpyo_container
 docker exec -it jwpyo_container /bin/zsh
 
